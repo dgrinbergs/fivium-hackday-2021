c15640cfe11df198da4a783c515157a889d48776
dainis <www.dainis.lv@gmail.com>
2021-04-16 10:00:22 +0100

    initial commit and prep

---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express')
+let socket = require('socket.io')
+
+// App setup
+let app = express()
+const port = 8080
+let server = app.listen(port, function () {
+    console.log(`Server started on port ${port}`)
+})
+
+app.use(express.static('static'))
+
+let users = new Map()
+
+let io = socket(server)
+io.on('connection', socket => {
+    io.to(socket.id).emit('members', Array.from(users.values()))
+
+    socket.on('disconnect', () => {
+        if (users.has(socket.id)) {
+            let name = users.get(socket.id)
+
+            io.sockets.emit('unregister', name)
+            console.log(`Disonnected: ${name}`)
+
+            users.delete(socket.id)
+            console.log(users)
+        }
+    })
+
+    socket.on('register', name => {
+        users.set(socket.id, name)
+        io.sockets.emit('register', name)
+
+        console.log(`Registered: ${name}`)
+        console.log(users)
+    })
+
+    socket.on('unregister', () => {
+        let name = users.get(socket.id)
+        io.sockets.emit('unregister', name)
+        users.delete(socket.id)
+
+        console.log(`Unregistered: ${name}`)
+        console.log(users)
+    })
+})
---
+++ static/prototype.js
@@ -0,0 +1,58 @@
+window.FontAwesomeConfig = { autoReplaceSvg: 'nest' }
+
+let Vue = new window.Vue({
+  el: '#app',
+  created () {
+    // establish connection
+    this.socket = io.connect()
+    fetch('/noodle-maps.json')
+      .then(r => r.json())
+      .then(data => {
+        this.location = data
+        console.log(data);
+      });
+  },
+  data: function() {
+    return {
+      errors: [],
+      socket: null,
+      registered: false,
+      name: "",
+      members: [],
+      location: {}
+    }
+  },
+  methods: {
+    connect: function () {
+      this.errors = []
+      if (this.name.length < 3) {
+        console.log(this.name.length > 2)
+        this.errors.push("Please enter your name")
+        return
+      }
+
+      this.socket.emit('register', this.name)
+      this.registered = true;
+    },
+    disconnect: function() {
+      this.socket.emit('unregister')
+      this.registered = false;
+    },
+    myFunction: function () {
+      console.log('can access data outside of mounted')
+    },
+  },
+  mounted: function() {
+    this.socket.on('register', name => {
+      this.members.push(name)
+    })
+
+    this.socket.on('unregister', name => {
+      this.members.splice(this.members.indexOf(name), 1)
+    })
+
+    this.socket.on('members', members => {
+      this.members = members;
+    })
+  }
+});
